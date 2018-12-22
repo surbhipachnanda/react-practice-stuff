7f1731b869b10fb0e5906e168d34fbd1d9b6bb59
surbhipachnanda <surbhi.pachnanda@gmail.com>
2018-12-22 12:08:47 +0530

    leaderboardLinking

--- my-react-practice/Code-Portal/client/assets/index.js
+++
@@ -1,3 +0,0 @@
-export { default as avatar } from './avatar.png';
-export { default as bigblacktrans } from './bigblacktrans.png';
-export { default as iecselogo } from './iecselogo.svg';
--- my-react-practice/Code-Portal/client/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const common = require('./build/webpack.common.js');
-
-module.exports = env => {
-	const configPath = `./build/webpack.${env || 'dev'}.js`;
-	console.log(`Using ${configPath}`);
-	return Object.assign(common, require(configPath));
-};
